c2530471cc24bb3702666a05db1af0f0d74ed024
Mykaela123 <stoffelsmykaela20@gmail.com>
2023-10-06 09:03:02 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GuessingGame2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(26, 35);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 46);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(26, 242);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(111, 54);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(651, 290);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 39);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(477, 290);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(119, 39);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblHint
+            // 
+            this.lblHint.AutoSize = true;
+            this.lblHint.Location = new System.Drawing.Point(49, 101);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(26, 13);
+            this.lblHint.TabIndex = 4;
+            this.lblHint.Text = "Hint";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(26, 138);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.TextBox txtAnswer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       public  bool playAgain = false;
+
+        public int currentHintIndex = 0;
+
+        public int score = 0;
+
+        string[] hints =
+         {
+                "This dark wizard who is the primary antagonist throughout the Harry Potter series. ",
+                "What magical creature guards the entrance to Professor Dumbledore's office?",
+                "The main trio of Harry, Ron, and Hermione often enjoys this beverage in the Hogwarts dining hall. It can make you feel warm and cozy.",
+                " This spell is used to summon an object to the caster's hand. What is the incantation for this spell?",
+                "Which Hogwarts house values traits such as bravery, chivalry, and courage?",
+                "The train that takes students to Hogwarts departs from this platform at King's Cross Station. What is the platform number?",
+                "This magical object, given to Harry by Professor Dumbledore, can be used to view the memories of the person who placed them in it.",
+                "Hagrid's beloved pet is a magical creature that resembles a giant spider. What is the name of this creature?",
+                "This location is the hidden entrance to the wizarding bank, Gringotts. It's guarded by a fierce dragon. What is the name of this place?",
+                "The sport played on broomsticks in the wizarding world is called what?"
+            };
+        private Random random = new Random();
+
+
+
+        private void RandomizeHints()
+        {
+            currentHintIndex = random.Next(0, hints.Length);
+            string randomHints = hints[currentHintIndex];
+            lblHint.Text = randomHints;
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtAnswer.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to Exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Close(); 
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            RandomizeHints();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string[] answers =
+                    {
+             "Voldemort",
+             "Phoenix ",
+             " Butterbeer",
+             "Accio",
+             "Gryfindor",
+             "Platform 9 3/4",
+             "Pensieve",
+             "Aragog",
+             "Diagon Alley",
+             "Quidditch"
+            };
+
+            string userAnswer = txtAnswer.Text.Trim();
+            if (currentHintIndex >= 0 && currentHintIndex < answers.Length)
+            {
+                string correctAnswer = answers[currentHintIndex].Trim();
+                if (userAnswer.Equals(correctAnswer, StringComparison.OrdinalIgnoreCase))
+                {
+                    score++;
+                    DialogResult response = MessageBox.Show("Correct!Your score is : " + score + " You are as smart as Hermonie.", "Answer", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    if (response == DialogResult.OK)
+                    {
+                        currentHintIndex++;
+
+
+                        if (currentHintIndex < hints.Length)
+                        {
+                            RandomizeHints();
+                        }
+                    }
+                }
+                else
+                {
+                    DialogResult response = MessageBox.Show("Incorrect!Your final score is : " + score, "Answer", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    if (response == DialogResult.OK)
+                    {
+                        DialogResult playAgainInput = MessageBox.Show("Would you like to try again?", "Game Over!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+                        if (playAgainInput == DialogResult.Yes)
+                        {
+                            playAgain = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
